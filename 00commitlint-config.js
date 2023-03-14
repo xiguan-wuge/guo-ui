70a2dc788eb65741ece06cbd4c179370ec0cd34d
xiguan-wuge <1584077483@qq.com>
2023-03-14 21:11:50 +0800

    测试文件

---
+++ 00commitlint-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // 继承默认配置
+  extends: [
+    "@commitlint/config-angular"
+  ],
+  // 自定义规则
+  // 规则由键值和配置数组组成，
+  // 如：'name: [0, 'always', 72]'，
+  // 数组中第一位为 level（等级），可选 0, 1, 2，0 为 disable（禁用），1 为 warning（警告），2 为 error（错误），
+  // 第二位为该规则是否被应用，可选 always | never， 
+  // 第三位为该规则允许值。
+  rules: {
+    'type-enum': [2, 'always', [
+      'upd',
+      'feat',
+      'fix',
+      'refactor',
+      'docs',
+      'chore',
+      'style',
+      'revert',
+    ]],
+    'header-max-length': [0, 'always', 72]
+  }
+};
